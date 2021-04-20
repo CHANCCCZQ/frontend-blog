6d8ae7f6e277ed82da8f1d303cc698ea41554c15
chenzhiqiang <chenzhiqiang@zuzuche.com>
2021-04-20 11:28:31 +0800

    🎉

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  title: '客服前端技术博客',
+  description: '成为为自己感到骄傲的那个人吧',
+  themeConfig: {
+    // logo: '/app.png',
+    nav: [
+      { text: '技术', link: '/skill/' },
+      { text: '业务', link: '/business/' },
+      { text: 'VuePress', link: 'https://www.vuepress.cn' },
+    ],
+    sidebar: [
+      {
+        title: '技术',   // 必要的
+        path: '/skill/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        // collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+          '/skill/crudPage',
+          '/skill/standard',
+          '/skill/interface'
+        ]
+      },
+      {
+        title: '业务',   // 必要的
+        path: '/business/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        // collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+          // '/business/crudPage'
+        ]
+      },
+    ],
+    lastUpdated: 'Last Updated',
+    smoothScroll: true
+  },
+
+  markdown: {
+    lineNumbers: true
+  },
+  port: 9999,
+  plugins: [
+    '@vuepress/active-header-links',
+    '@vuepress/back-to-top',
+    '@vuepress/nprogress',
+  ],
+  evergreen: true,
+}
---
+++ docs/skill/crudPage.md
@@ -0,0 +1,23 @@
+---
+title: JSON-SCHEMA 的 CRUD 页面配置
+description: 基于json的形式去快速生成一个crud页面
+lang: zh-CN
+---
+
+<!-- [[toc]] -->
+
+## 概述
+
+**由于 crud 是十分套路的页面，所以考虑抽取出共性的东西，使用 json 的形式去渲染页面，同时考虑提供可视化配置达到无代码的目的**
+
+| 配置项        |     说明      |   示例 |
+| ------------- | :-----------: | -----: |
+| col 3 is      | right-aligned | \$1600 |
+| col 2 is      |   centered    |   \$12 |
+| zebra stripes |   are neat    |    \$1 |
+
+## header 配置
+
+## table 配置
+
+## dialog 配置
